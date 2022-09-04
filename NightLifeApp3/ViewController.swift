84d9efaa372e6cf1a68409ca5f3649f83ed293c1
nkem-a <nkemanizoba@gmail.com>
2022-09-04 13:40:55 +0100

    Initial Commit

---
+++ NightLifeApp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NightLifeApp3
+//
+//  Created by user on 04/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
